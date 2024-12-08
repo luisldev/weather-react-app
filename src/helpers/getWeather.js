07224d38c83509792a8e4f786ce15acf8b0c375a
Luis Antonio López Rivera <luislopez2301@outlook.es>
2024-12-08 17:44:38 -0600

    Config time format

--- src/helpers/getWeather.js
+++ src/helpers/getWeather.js
@@ -4,20 +4,19 @@ async function getWeather(location = "Oaxaca"){
     const { lat, lon, API_KEY } = await getGeolocation(location)
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=es`)
     const data = await response.json()
+    const date = new Date(data.dt * 1000)
     const weather = {
         main: data.weather[0].main,
         description: data.weather[0].description,
         temp: data.main.temp,
         feels_like: data.main.feels_like,
-        temp_min: data.main.temp_min,
-        temp_max: data.main.temp_max,
         pressure: data.main.pressure,
         humidity: data.main.humidity,
         visibility: data.visibility,
         wind: data.wind.speed,
         wind_deg: data.wind.deg,
         clouds: data.clouds.all,
-        time: data.dt,
+        time: date.getHours() + ":" + date.getMinutes(),
         country: data.sys.country,
         city: data.name
     }
