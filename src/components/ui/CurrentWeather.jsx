f4e7bad7e1c21e4b39894f719984a2350af0f2d1
Luis Antonio López Rivera <luislopez2301@outlook.es>
2024-12-08 17:43:27 -0600

    Initialized current weather component

---
+++ src/components/ui/CurrentWeather.jsx
@@ -0,0 +1,13 @@
+
+
+function CurrentWeather(data) {
+  return (
+    <div className="weather_current rounded-lg">
+
+        <p>Current Weather</p>
+    
+    </div>
+  )
+}
+
+export default CurrentWeather
